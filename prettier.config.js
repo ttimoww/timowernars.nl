ec5538bf0d2432b9fee26edb2d2b8254e7f53ddf
Timo <ttimoww@gmail.com>
2023-03-12 14:29:54 +0100

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,25 @@
+/** @type {import('prettier').Config} */
+
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  printWidth: 180,
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '^.contentlayer/(.*)$',
+    '<THIRD_PARTY_MODULES>',
+    '^@/app/(.*)$',
+    '^@/config/(.*)$',
+    '^@/components/(.*)$',
+    '^@/styles/(.*)$',
+    '^[./]'
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true
+};
